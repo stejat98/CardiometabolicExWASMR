18d093404e20e058673fa8057619b1e3874eb80b
stejat98 <stejat@icloud.com>
2024-10-18 09:22:08 -0400

    Update Cox_CAD_binary_exposure_ExWAS.R

--- ExWAS/Incident_analysis/Cox_CAD_binary_exposure_ExWAS.R
+++ ExWAS/Incident_analysis/Cox_CAD_binary_exposure_ExWAS.R
@@ -8,18 +8,18 @@ library(survival)
 
 
 
-source("/home/st320/Baseline_PEWAS_Cox_Functions_script.R") 
+source("Baseline_PEWAS_Cox_Functions_script.R") 
 
 
-load("/home/st320/UKB_PEWAS/adjustments_survival_analysis.Rdata")
+load("adjustments_survival_analysis.Rdata")
 
 
-data <- read.fst("/n/groups/patel/sivateja/UKB/all_cause_mort_data_exposures_full.fst")
+data <- read.fst("incident_ExWAS_input_data.fst")
 data <- data %>% filter(f.21000.0.0 %in% c("British","Irish", "Any other white background"))
 
 adjustments <- c(adjustments, "x.738")
 
-load("/home/st320/UKB_PEWAS/binary_exposures.Rdata")
+load("binary_exposures.Rdata")
 
 exposures <- binvals[-length(binvals)]
 
@@ -29,4 +29,4 @@ for (exposureCol in exposures){
 
 ## run EWAS for specified phenotype (Baseline covariates) [extract p-values, etc.]
 EWASCoxph(data=data, depvar = "cad"
-          ,adjustments = adjustments,exposures=exposures,outFileName = sprintf("/n/groups/patel/sivateja/UKB/PEWAS_results/Coxph_Binary_Exposures_Baseline_Actual_06_28_23_Baseline_Pheno%sOverall_biglm_results", "CAD"))
+          ,adjustments = adjustments,exposures=exposures,outFileName = sprintf("Coxph_Binary_Exposures_Baseline_Actual_06_28_23_Baseline_Pheno%sOverall_biglm_results", "CAD"))
