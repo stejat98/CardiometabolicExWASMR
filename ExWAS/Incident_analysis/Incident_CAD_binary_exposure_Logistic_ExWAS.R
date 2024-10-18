62bab37dc22b1badd69dca24ce845f60ea72a993
stejat98 <stejat@icloud.com>
2024-10-18 09:31:10 -0400

    Update Incident_CAD_binary_exposure_Logistic_ExWAS.R

--- ExWAS/Incident_analysis/Incident_CAD_binary_exposure_Logistic_ExWAS.R
+++ ExWAS/Incident_analysis/Incident_CAD_binary_exposure_Logistic_ExWAS.R
@@ -5,19 +5,19 @@ library(biglm)
 library(broom)
 library(fst)
 
-source("/home/st320/Baseline_PEWAS_Logistic_Functions_script.R") 
+source("Baseline_PEWAS_Logistic_Functions_script.R") 
 
 
 
-load("/home/st320/UKB_PEWAS/adjustments_survival_analysis.Rdata")
+load("adjustments_survival_analysis.Rdata")
 
-data <- read.fst("/n/groups/patel/sivateja/UKB/all_cause_mort_data_exposures_full.fst")
+data <- read.fst("incident_ExWAS_input_data.fst")
 
 data <- data %>% filter(f.21000.0.0 %in% c("British","Irish", "Any other white background"))
 
 
 
-load("/home/st320/UKB_PEWAS/binary_exposures.Rdata")
+load("binary_exposures.Rdata")
 
 exposures <- binvals[-length(binvals)]
 
@@ -28,4 +28,4 @@ for (exposureCol in exposures){
 
 ## run EWAS for specified phenotype (Baseline covariates) [extract p-values, etc.]
 EWASLogistic(data=data, depvar = "cad"
-             ,adjustments = adjustments,exposures=exposures,outFileName = sprintf("/n/groups/patel/sivateja/UKB/PEWAS_results/Binary_Exposures_Baseline_Actual_03_29_22_Baseline_Pheno%sOverall_biglm_results", "CAD"))
+             ,adjustments = adjustments,exposures=exposures,outFileName = sprintf("Binary_Exposures_Baseline_Actual_03_29_22_Baseline_Pheno%sOverall_biglm_results", "CAD"))
